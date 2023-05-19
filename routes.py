431d664a9ae91de1dfdf6a214033eaddcaa386e0
nicokangdra <99784038+nicokangdra@users.noreply.github.com>
2023-05-19 10:08:55 +0700

    Add files via upload

---
+++ routes.py
@@ -0,0 +1,41 @@
+from databases.db import *
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    a=read_data("users")
+    return a
+
+@app.post("/add")
+def proot(items:dict):
+    collec = items["collec"]
+    doc = items["doc"]
+    data = items["data"]
+
+    add_data(collec,doc,data)
+    a = read_data("users",doc)
+    return {"message" : a}
+
+@app.post("/delete")
+def deletes(items:dict):
+    collec = items["collec"]
+    doc = items["doc"]
+
+    delete_data(collec,doc)
+    a = read_data("users")
+    return {"message" : a}
+
+@app.post("/update")
+def update(items:dict):
+    collec = items["collec"]
+    doc = items["doc"]
+    field = items['field']
+
+    update_data(collec,doc,field)
+    a = read_data(collec)
+    return {"message" : a}
+
+def fastapp():
+    return app
